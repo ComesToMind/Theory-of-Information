e1bdee08113a42a9a785ad0402c4e959a04b4528
CoderFromRussia <danilaoneal@gmail.com>
2020-05-10 01:02:54 +0700

    6 lab begin1

--- TI_lab6/Encode.py
+++ TI_lab6/Encode.py
@@ -10,25 +10,23 @@ def decimals_to_binaries():
     decimals: str = input("Введите последовательность цифр: ")
     i = 0
     split_by_three_to_bin: str = ""
-    while i < len(decimals):
+    for i in range(i, len(decimals), 3):
         split_by_three = decimals[i:i+3:1]
         if len(split_by_three) == 3:
             temp = bin(int(split_by_three))[2:]  # обрезаем у bin() 0b в начале
             split_by_three_to_bin +=(temp.rjust(10, '0')) # дополняет до 10 бит и кидаем в
-            i += 3
             continue
         if len(split_by_three) == 2:
             temp = bin(int(split_by_three))[2:]
             split_by_three_to_bin += (temp.rjust(7, '0'))  # дополняет до 7 бит и кидаем в
-            i += 3
             continue
         if len(split_by_three) == 1:
             temp = bin(int(split_by_three))[2:]  # обрезаем у bin() 0b в начале
             split_by_three_to_bin +=(temp.rjust(4, '0'))  # дополняет до 4 бит и кидаем в
-            i +=3
             continue
     return split_by_three_to_bin
 
 
 print(decimals_to_binaries())
+print("000111101101110010001001110")
 
