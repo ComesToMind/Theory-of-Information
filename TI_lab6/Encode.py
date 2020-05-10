9070181cb86da19671e8ccc2423573a937192a44
CoderFromRussia <danilaoneal@gmail.com>
2020-05-11 00:16:52 +0700

    qr_v0.5

--- TI_lab6/Encode.py
+++ TI_lab6/Encode.py
@@ -7,8 +7,9 @@ indexes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D',
 # Из этой таблицы определется версия нашего QR кода.
 
 # здесь лежат версии с 1-9 с уровнем сжатия М(15%)
-versies = [128,224,352,512,688,864,992,1232,1456]
-V : int = -1
+VERSIES = [128, 224, 352, 512, 688, 864, 992, 1232, 1456]
+AMOUNT_OF_BLOCKS = [1, 1, 1, 2, 2, 4, 4, 4, 5]
+V: int = -1
 # здесь храним посчитанный номер версии
 
 
@@ -37,7 +38,7 @@ def decimals_to_binaries():
             temp = bin(int(split_by_three))[2:]  # обрезаем у bin() 0b в начале
             split_by_three_to_bin +=(temp.rjust(4, '0'))  # дополняет до 4 бит и кидаем в
             continue
-    return add_type_and_length(split_by_three_to_bin, '0010') # добавили служебную информацию
+    return add_type_and_length(split_by_three_to_bin, '0001') # добавили служебную информацию
 
 
 def decimalsANDsymbols_to_binaries():
@@ -65,48 +66,67 @@ def decimalsANDsymbols_to_binaries():
             continue
     return add_type_and_length(split_by_two_to_bin, '0010') # добавили служебную информацию
 
-def add_type_and_length(initial_binaries : str, type: str)
+
+def add_type_and_length(initial_binaries : str, t : str):
+
     # длина количества поля данных (initial_binaries)
     # цифровое 10 бит
     # буквенно-циф 9 бит
 
     # в будущем проверить на слишком большое кол-во байт
     dec = len(initial_binaries)
-    if str == '0010':
+    if t == '0010':
         dec_str = (bin(dec)[2:0]).rjust(9,'0')
         return "0010" + dec_str + initial_binaries
-    if str == '0001':
+    if t == '0001':
         dec_str = (bin(dec)[2:0]).rjust(10, '0')
         return "0001" + dec_str + initial_binaries
 
 
 def fill_to_certain_length(for_fill: str):
+    # заполнение до длины равной ближайщей версии
     addit_byte1 = "11101100"
     addit_byte2 = "00010001"
     # делаем последовательность кратной 8
-    while for_fill % 8 != 0:
+    while len(for_fill) % 8 != 0:
         for_fill += '0'
     V = -1
     # ищим подходящую версию по длине бит
     i = 0
     for i in range(0,9):
-        if (versies[i]-len(for_fill)) >= 0:
+        if (VERSIES[i] - len(for_fill)) >= 0:
             V = i
             break
     i = 0
     # ПОТОМ ПРОВЕРИТЬ НА -1
-    while len(for_fill) != versies[V]:
+    while len(for_fill) != VERSIES[V]:
         if i % 2 == 0:
             for_fill += addit_byte1;
             i +=1
             continue
-        for_fill +=addit_byte2
+        for_fill += addit_byte2
         i +=1
+    return for_fill
 
 
+def forming_blocks(whole_data: str):
+    # Разделение информации на блоки
+    # сначала делим все на блоки по 8 байт
+    number_of_bytes = len(whole_data)//8
+    remainder = number_of_bytes % AMOUNT_OF_BLOCKS[V]  # делим на количество блоков в версии, остаток есть блоки с доп байтом
+    bytes_in_one_block = number_of_bytes // AMOUNT_OF_BLOCKS[V]
+    blocks = []
+    
+
+
+
+    return
 
 for_fill: str = decimalsANDsymbols_to_binaries()
 ready_data: str = fill_to_certain_length(for_fill)
+
+
+print(ready_data)
 # print(decimalsANDsymbols_to_binaries())
 
 # print(decimals_to_binaries())
